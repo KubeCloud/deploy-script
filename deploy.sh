af681d27578764488991ae96b99f150ed5218af1
Kasper Nissen <kni@it-minds.dk>
2016-06-08 11:52:54 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -23,9 +23,9 @@ echo "hub.docker.com repository to push to:" ${REPO}
 echo "version to tag the container with besides:"  ${VERSION}
 
 echo 'FROM hypriot/rpi-java
-COPY '$JAR' /data/
+COPY '{$JAR}' /data/
 EXPOSE 8080
-CMD ["java", "-jar", "'$JAR'"]' > Dockerfile
+CMD ["java", "-jar", "'{$JAR}'"]' > Dockerfile
 
 echo
 echo "Containerizing the successful build"
