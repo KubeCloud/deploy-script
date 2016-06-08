982590ee0f3e944bfb7b553f69c4e0c9d0ccfcc6
Kasper Nissen <kni@it-minds.dk>
2016-06-08 11:03:00 +0200

    deploy script for Java spring applications on Raspberry pi added

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+for i in "$@"
+do
+case $i in
+    -v=*|--version=*)
+    VERSION="${i#*=}"
+    ;;
+    -r=*|--repo=*)
+    REPO="${i#*=}"
+    ;;
+    -j=*|--jar=*)
+    JAR="${i#*=}"
+    ;;
+esac
+done
+
+echo
+echo "hub.docker.com repository to push to:" ${REPO}
+echo "version to tag the container with besides:"  ${VERSION}
+
+echo 'FROM hypriot/rpi-java
+COPY '$JAR' /data/
+EXPOSE 8080
+CMD ["java", "-jar", "'$JAR'"]' > Dockerfile
+
+echo
+echo "Containerizing the successful build"
+echo "######### STEP 1 - BUILDING DOCKER CONTAINER #########"
+docker build -t ${REPO}:${VERSION} .  
+docker tag -f ${REPO}:${VERSION} ${REPO}:latest
+
+echo
+echo "######### STEP 2 - PUSHING CONTAINER TO HUB.DOCKER.COM #########"
+docker push ${REPO}:${VERSION}
+docker push ${REPO}:latest
+
+echo
+echo "######### ALL DONE! #########"
